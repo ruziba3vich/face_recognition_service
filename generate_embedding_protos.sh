b57aa6f0b410ad1c569c2d81ec2cab865a84a201
ruziba3vich <rruzibayevich@gmail.com>
2025-05-27 10:43:49 +0500

    feat frs: write script to generate protos

---
+++ generate_embedding_protos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# chmod +x generate_embedding_protos.sh
+
+set -e
+
+PROTO_DIR="./protos"
+
+OUT_DIR="./genprotos"
+
+mkdir -p $OUT_DIR
+
+protoc -I=$PROTO_DIR \
+  --go_out=$OUT_DIR \
+  --go-grpc_out=$OUT_DIR \
+  $PROTO_DIR/face_recognition_protos/embed.proto
+
+echo "protos generated successfully"
